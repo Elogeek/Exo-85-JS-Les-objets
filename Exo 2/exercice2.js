0dd181111dc4a6074b72feeb614e64f94fad82a6
Elogeek <68891109+Elogeek@users.noreply.github.com>
2020-12-15 16:09:17 +0100

    LES OBJETS LITTERAUX

--- Exo 2/exercice2.js
+++ Exo 2/exercice2.js
@@ -1,11 +1,11 @@
 let personne = {
     prenom : "James",
     nom : "Bond",
-           :
+    job: "agent secret FBI"
 };
 
 /* Afficher ci dessous la propriété metier de notre objet dans un alert */
 
-alert();
+alert(personne.job);
 
 
--- Exo 3/exercice3.js
+++ Exo 3/exercice3.js
@@ -1,13 +1,14 @@
 let personne = {
+
     "prenom" : "James",
     "nom" : "Bond",
-           :    ,
-            :
+    "code": "007",
+
 };
 
 /* Afficher ci dessous "mon nom est <propriété nom de notre objet>, <propriété prenom de notre objet>
  <propriété nom de notre objet>, agent <propriété nomDeCode de notre objet>" dans un alert */
 
-alert();
+alert( "mon nom est " +personne.prenom + personne.nom + " je suis " + personne.code);
 
 
